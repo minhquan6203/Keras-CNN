4f77a1b753d68b064c1c8b468831e6fd52c10d9a
quan nguyen <21521333@gm.uit.edu.vn>
2023-04-03 08:33:51 +0700

    DenseNet

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 from tensorflow import keras
 from tensorflow.keras import layers
 from keras.models import Model
-from keras.layers import Input, Conv2D, BatchNormalization, Activation, Add, MaxPooling2D, Flatten, Dense
+from keras.layers import Input, Conv2D, BatchNormalization, Activation, Add, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.merge import concatenate
 from keras.regularizers import l2
 
 class CNN_Model(object):
@@ -159,6 +161,102 @@ class CNN_Model(object):
 
 
 
+    def dense_block(self,x, blocks, growth_rate):
+        for i in range(blocks):
+            conv = Conv2D(4 * growth_rate, (1,1), padding='same', kernel_initializer='he_normal')(x)
+            conv = BatchNormalization(axis=3)(conv)
+            conv = Activation('relu')(conv)
+
+            conv = Conv2D(growth_rate, (3,3), padding='same', kernel_initializer='he_normal')(conv)
+            conv = BatchNormalization(axis=3)(conv)
+            conv = Activation('relu')(conv)
+
+            x = concatenate([x, conv], axis=3)
+
+        return x
+
+    def transition_layer(self,x, compression):
+        num_filters = int(x.shape.as_list()[-1] * compression)
+
+        x = Conv2D(num_filters, (1,1), padding='same', kernel_initializer='he_normal')(x)
+        x = BatchNormalization(axis=3)(x)
+        x = Activation('relu')(x)
+        x = AveragePooling2D((2,2), strides=(2,2))(x)
+
+        return x
+
+    def DenseNet(self, blocks=[6,12,24,16], growth_rate=32, compression=0.5):
+
+
+        inputs = Input(shape=(self.image_W, self.image_H, self.image_C))
+
+        x = Conv2D(64, (7,7), strides=(2,2), padding='same', kernel_initializer='he_normal')(inputs)
+        x = BatchNormalization(axis=3)(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D((3,3), strides=(2,2), padding='same')(x)
+
+        for i, num_blocks in enumerate(blocks):
+            x = self.dense_block(x, num_blocks, growth_rate)
+            if i != len(blocks) - 1:
+                x = self.transition_layer(x, compression)
+
+        x = BatchNormalization(axis=3)(x)
+        x = Activation('relu')(x)
+        x = AveragePooling2D((7,7))(x)
+        x = Dense(self.num_classes, activation='softmax')(x)
+
+        model = Model(inputs, x, name='DenseNet')
+
+        return model
+
+
+    def conv_block(inputs, num_filters, kernel_size=3, padding='same', strides=1, activation='relu'):
+        conv = Conv2D(num_filters, kernel_size, padding=padding, strides=strides)(inputs)
+        conv = BatchNormalization()(conv)
+        conv = Activation(activation)(conv)
+        conv = Conv2D(num_filters, kernel_size, padding=padding, strides=strides)(conv)
+        conv = BatchNormalization()(conv)
+        conv = Activation(activation)(conv)
+        return conv
+
+    def encoder_block(inputs, num_filters, pool_size=(2,2), dropout=0.2):
+        conv = conv_block(inputs, num_filters)
+        pool = MaxPooling2D(pool_size)(conv)
+        if dropout > 0.0:
+            pool = Dropout(dropout)(pool)
+        return conv, pool
+
+    def decoder_block(inputs, skip_features, num_filters, upsample_size=(2,2), dropout=0.2):
+        upsample = UpSampling2D(upsample_size)(inputs)
+        upconv = Conv2D(num_filters, 2, padding='same')(upsample)
+        upconv = BatchNormalization()(upconv)
+        skip_conn = concatenate([upconv, skip_features], axis=3)
+        conv = conv_block(skip_conn, num_filters)
+        if dropout > 0.0:
+            conv = Dropout(dropout)(conv)
+        return conv
+
+    def build_unet(input_shape, num_classes, base_filters=64, depth=4, dropout=0.2):
+        inputs = Input(input_shape)
+
+        skip_connections = []
+        conv = inputs
+        for i in range(depth):
+            conv, pool = encoder_block(conv, base_filters*(2**i), dropout=dropout)
+            skip_connections.append(conv)
+
+        conv = conv_block(pool, base_filters*(2**depth), dropout=dropout)
+
+        for i in reversed(range(depth)):
+            conv = decoder_block(conv, skip_connections[i], base_filters*(2**i), dropout=dropout)
+
+        outputs = Conv2D(num_classes, 1, activation='softmax')(conv)
+
+        model = Model(inputs=inputs, outputs=outputs, name='U-Net')
+        return model
+
+
+
     def define_model(self):
         
         if self.type_model=="AlexNet":
@@ -167,6 +265,8 @@ class CNN_Model(object):
             return self.VGG16()
         if self.type_model=="ResNet34":
             return self.ResNet34()
+        if self.type_model=="DenseNet":
+            return self.DenseNet()
 
         return self.LeNet5()
 
