67385346d4445816096b44743f461ccbc052d81c
quan nguyen <21521333@gm.uit.edu.vn>
2023-04-04 18:19:54 +0700

    fix evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -2,13 +2,15 @@ import argparse
 import os
 import tensorflow as tf
 from tensorflow import keras
-
+from sklearn.metrics import f1_score, confusion_matrix
 from model import CNN_Model
 from loaddata import LoadData
 
 
-def eval(args):
 
+
+def eval(args):
+    
     model = tf.keras.models.load_model(os.path.join(os.path.join(args.checkpoint_path, 'best_model.h5')))
     load_data = LoadData(args.image_W, args.image_H, args.batch_size)
     test_data = load_data(data_path=args.test_path)
@@ -17,7 +19,16 @@ def eval(args):
     eval_results = model.evaluate(test_data)
     accuracy = eval_results[-1]
 
-    return accuracy
+    # Calculate f1 score
+    y_true = test_data.labels
+    y_pred = model.predict(test_data).argmax(axis=-1)
+    f1 = f1_score(y_true, y_pred, average='macro')
+
+    # Calculate confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    return accuracy, f1, cm
+
 
 
 def parse_args(parser):
@@ -36,6 +47,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parse_args(parser)
 
-    # Evaluate model on test data and print results
-    accuracy = eval(args)
+# Evaluate model on test data and print results
+    accuracy, f1, cm = eval(args)
     print("Test accuracy: {}".format(accuracy))
+    print("Test f1 score: {}".format(f1))
+    print("Confusion matrix:")
+    print(cm)
+
+
