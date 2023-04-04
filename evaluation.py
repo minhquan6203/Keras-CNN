5c74b3e9d34a87a8c5cecdc4f6affdc76d440c9f
quan nguyen <21521333@gm.uit.edu.vn>
2023-04-04 18:26:58 +0700

    fix evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -2,15 +2,14 @@ import argparse
 import os
 import tensorflow as tf
 from tensorflow import keras
-from sklearn.metrics import f1_score, confusion_matrix
 from model import CNN_Model
 from loaddata import LoadData
 
-
-
+from sklearn.metrics import f1_score, confusion_matrix
+import numpy as np
 
 def eval(args):
-    
+
     model = tf.keras.models.load_model(os.path.join(os.path.join(args.checkpoint_path, 'best_model.h5')))
     load_data = LoadData(args.image_W, args.image_H, args.batch_size)
     test_data = load_data(data_path=args.test_path)
@@ -20,14 +19,20 @@ def eval(args):
     accuracy = eval_results[-1]
 
     # Calculate f1 score
-    y_true = test_data.labels
-    y_pred = model.predict(test_data).argmax(axis=-1)
+    y_true = []
+    y_pred = []
+    for images, labels in test_data:
+        y_true.extend(labels.numpy())
+        y_pred.extend(model.predict(images).argmax(axis=-1))
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
     f1 = f1_score(y_true, y_pred, average='macro')
 
     # Calculate confusion matrix
     cm = confusion_matrix(y_true, y_pred)
 
     return accuracy, f1, cm
+
 
 
 
